dcc433c9b6eb60c3c3cb339bfc060f0de726508d
AndreSiboli <andresiboli@outlook.com>
2024-12-03 14:45:35 -0300

    Solving a merge problem - v1.0.8

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   experimental: {
     appDir: true,
   },
@@ -14,16 +13,6 @@ const nextConfig = {
       },
     ],
   },
-=======
-  // experimental: {
-  //   appDir: true,
-  // },
-
-  images: { formats: ["image/avif", "image/webp"], remotePatterns: [{
-    protocol: 'https',
-    hostname: 'fakestoreapi.com'
-  }] },
->>>>>>> 26f78578383efa00b00704a88c66d2b1218f0d48
 };
 
 module.exports = nextConfig;
